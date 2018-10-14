bb6465eee415ca1ec862134eb128eb9abbbde55d
Radosław Miernik <radekmie@gmail.com>
2018-10-14 18:23:19 +0200

    Fixed draftEvaluateSimple weights.

--- Draft.nim
+++ Draft.nim
@@ -33,9 +33,9 @@ func draftEvaluateSimple * (state: State): DraftResult =
     return
       (card.attack + card.defense) / (card.cost + 5) +
       (if card.hasBreakthrough: 0.1 else: 0) +
-      (if card.hasCharge:       0.1 else: 0) +
-      (if card.hasDrain:        0.1 else: 0) +
-      (if card.hasGuard:        0.1 else: 0) +
-      (if card.hasLethal:       0.1 else: 0) +
-      (if card.hasWard:         0.1 else: 0)
+      (if card.hasCharge:       0.2 else: 0) +
+      (if card.hasDrain:        0.2 else: 0) +
+      (if card.hasGuard:        0.4 else: 0) +
+      (if card.hasLethal:       0.3 else: 0) +
+      (if card.hasWard:         0.2 else: 0)
   )
