3e9bf9a4c8203a110f677f8306bff6bb12f0b982
Radosław Miernik <radekmie@gmail.com>
2018-10-15 11:06:57 +0200

    Simplified Input.getStr.

--- Input.nim
+++ Input.nim
@@ -7,20 +7,12 @@ proc getLine * (input: Stream): string {.inline.} =
 proc getStr * (input: Stream): string =
   var data: string
   while true:
-    if input of StringStream:
-      let peek = input.peekChar
-      if peek == ' ' or peek == '\n' or peek == '\0':
-        if peek != '\0':
-          discard input.readChar
-        break
-      data.add(input.readChar)
-    else:
-      let peek = input.readChar
-      if peek == ' ' or peek == '\n':
-        if data == "":
-          continue
-        break
-      data.add(peek)
+    let peek = input.readChar
+    if peek == ' ' or peek == '\n' or peek == '\0':
+      if data == "":
+        continue
+      break
+    data.add(peek)
   data
 
 proc getInt * (input: Stream): int {.inline.} =
