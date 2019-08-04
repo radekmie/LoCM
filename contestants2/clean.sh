fd656a58f29bab87d71f31db15e17f79a7aa0932
Radosław Miernik <radekmie@gmail.com>
2019-08-04 12:34:06 +0200

    Added contestants2.

---
+++ contestants2/clean.sh
@@ -0,0 +1,15 @@
+# AntiSquid
+# Baseline1
+# Baseline2
+# Coac
+(cd Coac && rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile main)
+# CoacProphet
+(cd CoacProphet && rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile main)
+# Conrisc
+# Fabbiamo
+(cd Fabbiamo && rm -f main)
+# Marasbot
+(cd Marasbot && rm -f main)
+# UJIAgent1
+# UJIAgent2
+# UJIAgent3
